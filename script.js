ed39abe903bced3af1e3c20ef775c6e945f6049f
anshuman7negi <anshumannegi777@gmail.com>
2023-06-19 14:14:39 +0530

    Add function for close button

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ menuBtn.addEventListener('click', () => {
   const mobilePopup = document.querySelector('.mobile-menu');
   mobilePopup.classList.toggle('visible');
 });
+
+document.getElementById('close-btn').addEventListener('click',()=>{
+  const mobilePopup = document.querySelector('.mobile-menu');
+  mobilePopup.classList.toggle('visible');
+});
+
+document.querySelectorAll('.menu-list a') .addEventListener('click',()=>{
+  const mobilePopup = document.querySelector('.mobile-menu');
+  mobilePopup.classList.toggle('visible');
+});
