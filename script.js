30a9ec52a1a668cfc2c66fcfd9641d793dee477a
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-06-19 13:50:02 +0500

    Write a function for link buttons in the popup

--- script.js
+++ script.js
@@ -9,7 +9,11 @@ document.getElementById('close-btn').addEventListener('click',()=>{
   mobilePopup.classList.toggle('visible');
 });
 
-document.querySelectorAll('.menu-list a') .addEventListener('click',()=>{
-  const mobilePopup = document.querySelector('.mobile-menu');
-  mobilePopup.classList.toggle('visible');
-});
+const linkBtn = document.querySelectorAll('.menu-list a');
+console.groupCollapsed(linkBtn.length);
+for (let i = 0; i < linkBtn.length; i += 1) {
+  linkBtn[i].addEventListener('click',()=>{
+    const mobilePopup = document.querySelector('.mobile-menu');
+    mobilePopup.classList.toggle('visible');
+  });
+}
