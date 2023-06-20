138a3c977f4e919c6f52d30e17e1123fac220705
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-06-20 15:11:56 +0500

    Create popup.js and write an array of objects for my project details

---
+++ popup.js
@@ -0,0 +1,56 @@
+const projectDetails = [
+  {
+    "name": "Project name goes here",
+    "description": ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi", "Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."],
+    "featured_image": "./images/project-1-featured.svg",
+    "alternative_images": ["./images/project-1-small-1.svg", "./images/project-1-small-2.svg", "./images/project-1-small-3.svg", "./images/project-1-small-4.svg"],
+    "technologies": ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    "link_version": "https://zilola-nazarova.github.io/zilola-portfolio/",
+    "link_source": "https://github.com/Zilola-Nazarova/zilola-portfolio"
+  },
+  {
+    "name": "Project name goes here",
+    "description": ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi", "Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."],
+    "featured_image": "./images/project-2-featured.svg",
+    "alternative_images": ["./images/project-2-small-1.svg", "./images/project-2-small-2.svg", "./images/project-2-small-3.svg", "./images/project-2-small-4.svg"],
+    "technologies": ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    "link_version": "https://zilola-nazarova.github.io/zilola-portfolio/",
+    "link_source": "https://github.com/Zilola-Nazarova/zilola-portfolio"
+  },
+  {
+    "name": "Project name goes here",
+    "description": ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi", "Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."],
+    "featured_image": "./images/project-3-featured.svg",
+    "alternative_images": ["./images/project-3-small-1.svg", "./images/project-3-small-2.svg", "./images/project-3-small-3.svg", "./images/project-3-small-4.svg"],
+    "technologies": ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    "link_version": "https://zilola-nazarova.github.io/zilola-portfolio/",
+    "link_source": "https://github.com/Zilola-Nazarova/zilola-portfolio"
+  },
+  {
+    "name": "Project name goes here",
+    "description": ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi", "Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."],
+    "featured_image": "./images/project-4-featured.svg",
+    "alternative_images": ["./images/project-4-small-1.svg", "./images/project-4-small-2.svg", "./images/project-4-small-3.svg", "./images/project-4-small-4.svg"],
+    "technologies": ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    "link_version": "https://zilola-nazarova.github.io/zilola-portfolio/",
+    "link_source": "https://github.com/Zilola-Nazarova/zilola-portfolio"
+  },
+  {
+    "name": "Project name goes here",
+    "description": ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi", "Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."],
+    "featured_image": "./images/project-5-featured.svg",
+    "alternative_images": ["./images/project-5-small-1.svg", "./images/project-5-small-2.svg", "./images/project-5-small-3.svg", "./images/project-5-small-4.svg"],
+    "technologies": ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    "link_version": "https://zilola-nazarova.github.io/zilola-portfolio/",
+    "link_source": "https://github.com/Zilola-Nazarova/zilola-portfolio"
+  },
+  {
+    "name": "Project name goes here",
+    "description": ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi", "Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."],
+    "featured_image": "./images/project-6-featured.svg",
+    "alternative_images": ["./images/project-6-small-1.svg", "./images/project-6-small-2.svg", "./images/project-6-small-3.svg", "./images/project-6-small-4.svg"],
+    "technologies": ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    "link_version": "https://zilola-nazarova.github.io/zilola-portfolio/",
+    "link_source": "https://github.com/Zilola-Nazarova/zilola-portfolio"
+  }
+]
