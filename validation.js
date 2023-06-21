5416cf99cc15283d82350d1696515428b1e5d365
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-06-21 13:25:37 +0500

    Add functions for calling showMessage function with corresponding type values

--- validation.js
+++ validation.js
@@ -4,3 +4,11 @@ function showMessage(input, message, type) {
 	input.className = type ? "success" : "error";
 	return type;
 }
+
+function showError(input, message) {
+	return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+	return showMessage(input, "", true);
+}
