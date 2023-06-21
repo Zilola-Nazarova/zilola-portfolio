d35e258b0eae7016e7427881488f6b786b03d197
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-06-21 13:23:29 +0500

    Create empty rulesets for .success and .error classes of input

---
+++ validation.js
@@ -0,0 +1,6 @@
+function showMessage(input, message, type) {
+	const msg = input.parentNode.querySelector("small");
+	msg.innerText = message;
+	input.className = type ? "success" : "error";
+	return type;
+}
