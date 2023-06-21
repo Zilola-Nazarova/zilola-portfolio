52f093059c1bb7f3b7b4395e795ed93b7f6e838a
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-06-21 15:48:43 +0500

    Fix linter errors

--- validation.js
+++ validation.js
@@ -1,42 +1,37 @@
+const form = document.querySelector('#send-message');
+
 function showMessage(input, message, type) {
-	const msg = input.parentNode.querySelector("small:last-of-type");
-	const suggestEmail='Replace it with'+form.elements['user_email'].value.toLowerCase() + "?";
-	console.log(suggestEmail);
-	msg.innerText = message + suggestEmail;
-	input.className = type ? "success" : "error";
-	return type;
+  const msg = input.parentNode.querySelector('small:last-of-type');
+  const suggestEmail = `Replace it with ${form.elements.user_email.value.toLowerCase()} ?`;
+  msg.innerText = message + suggestEmail;
+  input.className = type ? 'success' : 'error';
+  return type;
 }
 
 function showError(input, message) {
-	return showMessage(input, message, false);
+  return showMessage(input, message, false);
 }
 
-function showSuccess(input) {
-	return showMessage(input, "", true);
-}
+// function showSuccess(input) {
+//   return showMessage(input, '', true);
+// }
 
 function validateEmail(input, invalidMsg) {
-	const emailRegex = /^[^A-Z\s]+\.[^A-Z\s]+$/;
-	const email = input.value.trim();
-	if (!emailRegex.test(email)) {
-		return showError(input, invalidMsg);
-	}
-	return true;
+  const emailRegex = /^[^A-Z\s]+\.[^A-Z\s]+$/;
+  const email = input.value.trim();
+  if (!emailRegex.test(email)) {
+    return showError(input, invalidMsg);
+  }
+  return true;
 }
 
-const form = document.querySelector("#send-message");
+const EMAIL_LOWERCASE = 'Please enter your email in lowercase.';
 
-const EMAIL_LOWERCASE = "Please enter your email in lowercase.";
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-form.addEventListener("submit", function (event) {
-	// stop form submission
-	event.preventDefault();
-
-	// validate the form
-	let emailValid = validateEmail(form.elements["user_email"], EMAIL_LOWERCASE);
-	// if valid, submit the form.
-	if (emailValid) {
-		form.submit();
-	}
+  const emailValid = validateEmail(form.elements.user_email, EMAIL_LOWERCASE);
+  if (emailValid) {
+    form.submit();
+  }
 });
-
