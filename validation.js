fc29fca414dc2b228237097e82dca3dfa2c2afed
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-06-21 16:23:45 +0500

    Adjust the height of the <small> message to be flexible, add a space between two sentences of the error message

--- validation.js
+++ validation.js
@@ -2,7 +2,7 @@ const form = document.querySelector('#send-message');
 
 function showMessage(input, message, type) {
   const msg = input.parentNode.querySelector('small:last-of-type');
-  const suggestEmail = `Replace it with ${form.elements.user_email.value.toLowerCase()} ?`;
+  const suggestEmail = ` Replace it with ${form.elements.user_email.value.toLowerCase()}?`;
   msg.innerText = message + suggestEmail;
   input.className = type ? 'success' : 'error';
   return type;
