75f8b75adf044cb03c9bb18883ded8ae5293d1c0
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-06-22 18:09:20 +0500

    Fix eslint errors

--- local-storage.js
+++ local-storage.js
@@ -1,69 +1,58 @@
+/* eslint-disable consistent-return */
 function storageAvailable(type) {
   let storage;
   try {
     storage = window[type];
-    const x = "__storage_test__";
+    const x = '__storage_test__';
     storage.setItem(x, x);
     storage.removeItem(x);
     return true;
   } catch (e) {
     return (
-      e instanceof DOMException &&
+      e instanceof DOMException
       // everything except Firefox
-      (e.code === 22 ||
+      && (e.code === 22
         // Firefox
-        e.code === 1014 ||
+        || e.code === 1014
         // test name field too, because code might not be present
         // everything except Firefox
-        e.name === "QuotaExceededError" ||
+        || e.name === 'QuotaExceededError'
         // Firefox
-        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+        || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
       // acknowledge QuotaExceededError only if there's something already stored
-      storage &&
-      storage.length !== 0
+      && storage
+      && storage.length !== 0
     );
   }
 }
 
-if (storageAvailable("localStorage")) {
-  console.log('Storage is available');
-} else {
-  console.log('Storage unavailable');
-}
+function setFormValues() {
+  const currentName = localStorage.getItem('name');
+  const currentEmail = localStorage.getItem('email');
+  const currentMessage = localStorage.getItem('message');
 
-
-
-
-console.log(localStorage.length);
-console.log(localStorage);
-
-if (localStorage.length < 1) {
-  populateStorage();
-} else {
-  setFormValues();
+  document.getElementById('user-name').value = currentName;
+  document.getElementById('user-email').value = currentEmail;
+  document.getElementById('user-message').value = currentMessage;
 }
 
 function populateStorage() {
-  localStorage.setItem("name", document.getElementById("user-name").value);
-  localStorage.setItem("email", document.getElementById("user-email").value);
-  localStorage.setItem("message", document.getElementById("user-message").value);
- 
+  localStorage.setItem('name', document.getElementById('user-name').value);
+  localStorage.setItem('email', document.getElementById('user-email').value);
+  localStorage.setItem('message', document.getElementById('user-message').value);
   setFormValues();
 }
 
-function setFormValues() {
-  const currentName = localStorage.getItem("name");
-  const currentEmail = localStorage.getItem("email");
-  const currentMessage = localStorage.getItem("message");
-
-  document.getElementById("user-name").value = currentName;
-  document.getElementById("user-email").value = currentEmail;
-  document.getElementById("user-message").value = currentMessage;
-
+if (storageAvailable('localStorage')) {
+  if (localStorage.length < 1) {
+    populateStorage();
+  } else {
+    setFormValues();
+  }
+} else {
+  return false;
 }
 
-document.getElementById("user-name").onchange = populateStorage;
-document.getElementById("user-email").onchange = populateStorage;
-document.getElementById("user-message").onchange = populateStorage;
-
-
+document.getElementById('user-name').onchange = populateStorage;
+document.getElementById('user-email').onchange = populateStorage;
+document.getElementById('user-message').onchange = populateStorage;
