e52d4c9fe6750d4c4b9f604bf779c0354b99395f
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-06-22 20:07:25 +0500

    Uncomment the storageAvailable function and the if-statement to check for the availability of the storage

--- local-storage.js
+++ local-storage.js
@@ -1,48 +1,47 @@
 /* eslint-disable consistent-return */
-// function storageAvailable(type) {
-//   let storage;
-//   try {
-//     storage = window[type];
-//     const x = '__storage_test__';
-//     storage.setItem(x, x);
-//     storage.removeItem(x);
-//     return true;
-//   } catch (e) {
-//     return (
-//       e instanceof DOMException
-//       // everything except Firefox
-//       && (e.code === 22
-//         // Firefox
-//         || e.code === 1014
-//         // test name field too, because code might not be present
-//         // everything except Firefox
-//         || e.name === 'QuotaExceededError'
-//         // Firefox
-//         || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
-//       // acknowledge QuotaExceededError only if there's something already stored
-//       && storage
-//       && storage.length !== 0
-//     );
-//   }
-// }
-
-// if (storageAvailable('localStorage')) {
-//   if (localStorage.length < 1) {
-//     populateStorage();
-//   } else {
-//     setFormValues();
-//   }
-// } else {
-//   return false;
-// }
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException
+      // everything except Firefox
+      && (e.code === 22
+        // Firefox
+        || e.code === 1014
+        // test name field too, because code might not be present
+        // everything except Firefox
+        || e.name === 'QuotaExceededError'
+        // Firefox
+        || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+      // acknowledge QuotaExceededError only if there's something already stored
+      && storage
+      && storage.length !== 0
+    );
+  }
+}
 
 const inputArray = document.getElementById('send-message');
 
-for (let i = 0; i < inputArray.length - 1; i++) {
-  // inputArray[i].onkeypress = populateStorage;
-  inputArray.elements[i].addEventListener('keypress',function() {
+if (storageAvailable('localStorage')) {
+  if (localStorage.length < 1) {
     populateStorage();
-});
+  } else {
+    setInputValues();
+  }
+} else {
+}
+
+for (let i = 0; i < inputArray.length - 1; i++) {
+  inputArray[i].onkeypress = populateStorage;
+//   inputArray.elements[i].addEventListener('keypress',function() {
+//     populateStorage();
+// });
   function populateStorage() {
     localStorage.setItem(inputArray[i].name, inputArray[i].value);
   }
