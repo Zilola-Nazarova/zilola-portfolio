fb3f8716d613e8269c2f009106cf055f847f4937
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-07-21 15:39:41 +0500

    Change the path to the featured image of the first project

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ const projectDetails = [
   {
     name: 'Project name goes here',
     description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi', 'Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.'],
-    featured_image: './images/details-popup/project-1-featured.svg',
+    featured_image: './images/project-1.png',
     alternative_images: ['./images/details-popup/project-1-small-1.svg', './images/details-popup/project-1-small-2.svg', './images/details-popup/project-1-small-3.svg', './images/details-popup/project-1-small-4.svg'],
     technologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
     link_version: 'https://zilola-nazarova.github.io/capstone-project-module-1/',
